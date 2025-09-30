e5dd32a9fd59613b1461389a9f501fec624d86da
ahnaf.hasan <ahnaf.hasan@niftycoders.com>
2025-09-30 16:40:43 +0600

    clean frontend file

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,40 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    PasswordModule,
+    ButtonModule
+  ],
+
   templateUrl: './app.html',
+  styleUrls: ['./app.css']
 })
 export class App {
-  protected readonly title = signal('frontend');
+  repoForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.repoForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      repoName: ['', [Validators.required]],
+      accessToken: ['', [Validators.required, Validators.minLength(10)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.repoForm.valid) {
+      console.log('Form Data:', this.repoForm.value);
+      // You can add service call here
+    } else {
+      this.repoForm.markAllAsTouched();
+    }
+  }
 }
